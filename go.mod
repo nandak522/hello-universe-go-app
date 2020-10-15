fc994e59d2ca71ab7ed4bad128122222feceebeb
Nanda Kishore <battaru.nandakishore@bigbasket.com>
2020-10-15 14:18:47 +0530

    Start supporting configurable log level

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/none-da/hello-universe-go-app
 
-go 1.14
+go 1.15
 
 require (
 	github.com/labstack/echo/v4 v4.1.17
-	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	github.com/labstack/gommon v0.3.0
+	github.com/spf13/pflag v1.0.5
 )
--- server.go
+++ server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
+	flag "github.com/spf13/pflag"
 )
 
 // StartTime gives the start time of server
@@ -39,6 +40,15 @@ func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c 
 }
 
 func main() {
+	var requiredLogLevel string
+	flag.StringVarP(&requiredLogLevel, "log-level", "l", "info", "Required log level: debug/info/warn/error. Defaults to info")
+	var printHelp bool
+	flag.BoolVarP(&printHelp, "help", "h", false, "Prints this help content.")
+	flag.Parse()
+	if printHelp {
+		flag.Usage()
+		return
+	}
 	e := echo.New()
 	renderer := &TemplateRenderer{
 		templates: template.Must(template.ParseGlob("templates/*.html")),
@@ -51,6 +61,19 @@ func main() {
 			`"bytes_out":${bytes_out}}` + "\n",
 		Output: os.Stdout,
 	}))
+	switch requiredLogLevel {
+	case "debug":
+		e.Logger.SetLevel(log.DEBUG)
+	case "info":
+		e.Logger.SetLevel(log.INFO)
+	case "warn":
+		e.Logger.SetLevel(log.WARN)
+	case "error":
+		e.Logger.SetLevel(log.ERROR)
+	default:
+		e.Logger.SetLevel(log.INFO)
+	}
+	e.Logger.Debugf("Loglevel is set to %s", requiredLogLevel)
 	e.Logger.SetLevel(log.DEBUG)
 	var port, isEnvVarSet = os.LookupEnv("APP_PORT")
 	if !isEnvVarSet {
